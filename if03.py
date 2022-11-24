51c5d15c526186dad2759302aaf36503438dc76a
Muxtor <jalilovmuxtor203@gmail.com>
2022-11-24 12:11:48 +0500

    03

--- if03.py
+++ if03.py
@@ -8,4 +8,10 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    if a>b and b>c or c>b and b>a:
+        return b
+    if b>a and a>c or c>a and a>b:
+        return a
+    if a>c and c>b or b>c and c>a:
+        return c
+print(main(2,3,5))
