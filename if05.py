c49cefcdcce09986fb95f203e910634139ebff78
Muxtor <jalilovmuxtor203@gmail.com>
2022-11-24 11:17:20 +0500

    05

--- if05.py
+++ if05.py
@@ -6,4 +6,24 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    x1=n%10
+    n=n//10
+    x2=n%10
+    n=n//10
+    x3=n%10
+    n=n//10
+    x4=n%10
+    x5=n//10
+    y=0
+    if y<x1:
+        y=x1
+    if y<x2:
+        y=x2
+    elif y<x3:
+        y=x3
+    elif y<x4:
+        y=x4
+    elif y<x5:
+        y=x5
+    return y
+print(main(12345))
