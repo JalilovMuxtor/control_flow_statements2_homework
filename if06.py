e2ab078373337799c152bf71402137277fbd3c5e
Muxtor <jalilovmuxtor203@gmail.com>
2022-11-24 11:24:01 +0500

    6

--- if06.py
+++ if06.py
@@ -6,4 +6,19 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    
+    n1=n%10
+    n=n//10
+
+    n2=n%10
+    n=n//10
+
+    n3=n%10
+    n=n//10
+
+    n4=n%10
+    n=n//10
+
+    n5=n
+    return n1,n2,n3,n4,n5
+print(main(56892))
