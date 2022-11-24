aa4889fae83cf6d069b07a003069a3b2e5533ff7
Muxtor <jalilovmuxtor203@gmail.com>
2022-11-24 17:53:47 +0500

    06

--- if06.py
+++ if06.py
@@ -16,21 +16,15 @@ def main(n):
     x4=n%10
     n=n//10
     x5=n//10
-    mx=x1
-    i=4
-    if mx<x2:
-        mx=x2
-        i=3
-
-    if mx<x3:
-        mx=x3
-        i=2
-    if mx<x4:
-        mx=x4
-        i=1
-    if mx<x5:
-        mx=x5
-        i=0
-  
-    return i
+    
+    if x1>x2 and x1>x3 and x1>x4 and x1>x5:
+        return len(str(x1))
+    elif x2>x1 and x2>x3 and x2>x4 and x2>x5:
+        return len(str(x2*10+x1))
+    elif x3>x1 and x3>x2 and x3>x4 and x3>x5:
+        return len(str(x3*100+x1))
+    elif x4>x1 and x4>x2 and x4>x3 and x4>x5:
+        return len(str(x4*1000+x1))
+    elif x5>x1 and x5>x2 and x5>x3 and x5>x4:
+        return len(str(x5*10000+x1))
 print(main(15342))
