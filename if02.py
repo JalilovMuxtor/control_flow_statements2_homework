d34dc781dc9273a48738d360d6e0a13c553f6142
Muxtor <jalilovmuxtor203@gmail.com>
2022-11-24 14:18:34 +0500

    02.

--- if02.py
+++ if02.py
@@ -8,4 +8,16 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    
+    if a<b:
+        if a<c:
+            return(a)
+        else:
+            return(c)
+
+    else:
+        if b<c:
+            return(b)
+        else:
+            return(c) 
+print(main(5,6,3))
